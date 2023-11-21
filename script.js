6229504278588fa64af5a86b4bd438f0302a3bbb
ImAleeMMI <147595619+ImAleeMMI@users.noreply.github.com>
2023-11-21 12:51:59 +0100

    modificated file

--- script.js
+++ script.js
@@ -6,45 +6,63 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
+
+
 // Function to add a new task
 function addTask() {
     // recupero value dell'input
     var task = document.getElementById('new-task').value;
 
-    // Element container delle lista
-    var listContainer = document.getElementById('tasks-list');
-
-    // Creo Element li
-    var newTaskElem = document.createElement("li");
-    // Appendo Element li al container
-    var newTask = listContainer.appendChild(newTaskElem);
-
-    // Scrivo value dell'input nel li
-    newTask.innerHTML = task;
-
-    // Creo uno span
-    var checkElem = document.createElement("span");
-    checkElem.innerHTML = 'c';
-    checkElem.classList.add('icon-check');
-    newTask.id = 'task-' + creteUniqueID();
-    newTask.appendChild(checkElem);
+    if (task != '') {
+        // Element container delle lista
+        var listContainer = document.getElementById('tasks-list');
+        // Creo Element li
+        var newTaskElem = document.createElement("li");
 
 
+        // Appendo Element li al container
+        var newTask = listContainer.appendChild(newTaskElem);
+        newTaskElem.classList.add('task');
 
-    var removeElem = document.createElement("span");
-    removeElem.innerHTML = 'r';
-    removeElem.classList.add('icon-check');
-    newTask.appendChild(removeElem);
+        // Scrivo value dell'input nel li
+        newTask.innerHTML = task;
 
-    removeElem.addEventListener('click', function (e) {
-        removeTask(e.target);
-    });
+        const taskUrg = "urgente";
+        if (task.indexOf(taskUrg) !== -1) {
+            newTaskElem.classList.add('add-urg');
+        }
+        // Creo uno span
+        var removeElem = document.createElement("span");
+        removeElem.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+        removeElem.classList.add('icon-remove');
+        newTask.appendChild(removeElem);
 
-    checkElem.addEventListener('click', function (e) {
-        checkedTask(e.target, removeElem);
-    });
+        var checkElem = document.createElement("span");
+        checkElem.innerHTML = '<i class="fa-solid fa-check"></i>';
+        checkElem.classList.add('icon-check');
+        newTask.id = 'task-' + creteUniqueID();
+        newTask.appendChild(checkElem);
 
-    console.log(listContainer);
+        removeElem.addEventListener('click', function (e) {
+            removeTask(e.target);
+        });
+
+        checkElem.addEventListener('click', function (e) {
+            checkedTask(e.target, removeElem);
+
+        });
+        console.log(listContainer);
+
+        document.getElementById('new-task').value = "";
+        document.getElementById("error-message").innerHTML = "";
+        countTask();
+
+        const now = new Date();
+        // get the current date and time as a string
+        const currentDateTime = now.toLocaleString();
+        document.querySelector('#datetime').textContent = currentDateTime;
+        newTaskElem.innerHTML = `<span>${currentDateTime}</span>`;
+    }
 };
 
 function creteUniqueID() {
@@ -60,14 +78,32 @@ function creteUniqueID() {
 }
 
 function removeTask(elem) {
-
     if (confirm('Sei sicuto di voler cancellare questa task? ') == true) {
-        elem.parentNode.remove();
+        elem.parentNode.parentNode.remove();
+        countTask();
     }
 }
 
-function checkedTask(elem,removeElem) {
-    elem.parentNode.classList.add('checked');
+function checkedTask(elem, removeElem) {
+    elem.parentNode.parentNode.classList.add('checked'); console.log(elem.parentNode);
     removeElem.remove();
     elem.remove();
+    countTask();
+}
+
+function characterCounter(task) {
+    console.log(task);
+    maxLength = 50;
+    if (task.length > maxLength) {
+        document.getElementById('new-task').value = task.substring(0, maxLength);
+        document.getElementById("error-message").innerHTML = "Attenzione! Il numero massimo di caratteri è " + maxLength;
+    }
+}
+
+function countTask() {
+    var taskList = document.getElementById('tasks-list');
+    var totalTask = taskList.querySelectorAll('.task').length;
+    var checkedTask = taskList.querySelectorAll('.checked').length;
+    document.getElementById("count-total").innerHTML = totalTask;
+    document.getElementById("count-checked").innerHTML = checkedTask;
 }
